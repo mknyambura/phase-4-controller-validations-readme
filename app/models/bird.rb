1c8c2b8445c33e9e42c28db0df3fb40236e8fdf8
mknyambura <kariuki.mercie@gmail.com>
2023-03-16 19:41:23 +0300

    completed assignment

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,18 @@
 class Bird < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+
+    def create
+        bird = Bird.create!(bird_params)
+        render json: bird, status: :created
+      rescue ActiveRecord::RecordInvalid => invalid
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
+
+    def update
+        bird = find_bird
+        bird.update!(bird_params)
+        render json: bird
+      rescue ActiveRecord::RecordInvalid => invalid
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
 end
